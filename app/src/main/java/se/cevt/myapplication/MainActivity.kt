825c3e32224ae3ba3591ac224854982b0566093e
Sanaz Ramezani <sanaz.ramezani@cevt.se>
2019-11-22 12:15:30 +0100

    First chapter

--- app/src/main/java/se/cevt/myapplication/MainActivity.kt
+++ app/src/main/java/se/cevt/myapplication/MainActivity.kt
@@ -2,11 +2,30 @@ package se.cevt.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var trueButton : Button
+    private lateinit var falseButton : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        init()
+    }
+
+    private fun init() {
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
+
+        trueButton.setOnClickListener {
+
+        }
+
+        falseButton.setOnClickListener {
+
+        }
     }
 }
